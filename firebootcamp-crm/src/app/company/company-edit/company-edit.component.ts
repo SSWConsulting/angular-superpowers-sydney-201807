875c07d0eeca96f20efff8f8c278710e420e8a2a
JeanThirion <jean.thirion@esial.net>
2018-07-13 14:42:42 +1000

    Company Edit : Save & Update

--- firebootcamp-crm/src/app/company/company-edit/company-edit.component.ts
+++ firebootcamp-crm/src/app/company/company-edit/company-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { CompanyService } from 'src/app/company/company.service';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'fbc-company-edit',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyEditComponent implements OnInit {
 
-  constructor() { }
+  companyForm: FormGroup;
+  companyId: number;
+  isNewCompany: boolean;
+
+  constructor(
+    private router : Router,
+    private activatedRoute: ActivatedRoute,
+    private companyService : CompanyService,
+    private formBuilder : FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.companyId = ~~(this.activatedRoute.snapshot.params["id"]);
+    this.isNewCompany = this.companyId === 0;
+
+    this.buildForm();
+
+    if(!this.isNewCompany){
+      this.companyService.getCompany(this.companyId).subscribe(c => {
+        this.companyForm.patchValue(c);
+      })
+    }
+
+  }
+
+  buildForm(){
+    this.companyForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: [''],
+      phone: ['']
+    })
+  }
+
+  saveCompany(): void {
+    if (this.isNewCompany) {
+      this.companyService.addCompany(this.companyForm.value)
+        .subscribe(() => this.router.navigate(['/company/list']));
+    }else{
+      var companyItem = {...this.companyForm.value, id: this.companyId}
+      this.companyService.updateCompany(companyItem)
+      .subscribe(() => this.router.navigate(['/company/list']));
+    }
   }
 
 }
--- firebootcamp-crm/src/app/company/company.service.ts
+++ firebootcamp-crm/src/app/company/company.service.ts
@@ -3,6 +3,7 @@ import { Company } from 'src/app/company/company';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +25,24 @@ export class CompanyService {
 
   deleteCompany(id: number) : Observable<Company>{
     return this.httpClient.delete<Company>(`${this.API_BASE}/company/${id}`)
+  }
+
+  addCompany(company : Company){
+    return this.httpClient.post<Company>(
+      `${this.API_BASE}/company`, company, { headers: new HttpHeaders().set('content-type', 'application/json') }
+    ).pipe(catchError(this.errorHandler));
+  }
+
+  getCompany(companyId: number): Observable<Company> {
+    return this.httpClient.get<Company>(`${this.API_BASE}/company/${companyId}`)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  updateCompany(company: Company): Observable<Company> {
+    return this.httpClient.put(
+      `${this.API_BASE}/company/${company.id}`, company,
+      { headers: new HttpHeaders().set('content-type', 'application/json') }
+    ).pipe(catchError(this.errorHandler));
   }
 
   errorHandler(): Observable<any> {
